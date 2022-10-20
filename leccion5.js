9954e9cc7e988d20f1ab98d4a9859fa6d66f8caf
Carlos Tutusaus - Connecting Visions <ctutusaus@connectingvisionsgroup.com>
2022-10-20 11:42:59 +0200

    Feat:Leccion5

---
+++ leccion5.js
@@ -0,0 +1,11 @@
+const altura_cm = 180
+console.log(altura_cm);
+const altura_m = 1.79
+console.log(altura_m);
+const peso_kg = 66.2
+
+const altura_alza = Math.round(altura_m);
+const altura_baja = Math.floor(altura_m);
+
+const Max_Value = Number.MAX_VALUE + 1; 
+console.log(Max_Value)
