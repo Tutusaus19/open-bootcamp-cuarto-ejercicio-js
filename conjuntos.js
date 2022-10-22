c07843b675b2a1694ad9af2681b74c50a7dece46
Carlos Tutusaus - Connecting Visions <ctutusaus@connectingvisionsgroup.com>
2022-10-22 20:02:26 +0200

    Feat:colleciones.js

---
+++ conjuntos.js
@@ -0,0 +1,9 @@
+const lista = ["Carlos", "Pelayo", "Daniela", "olivia", "María", "Carlos"]
+
+const newSet = new Set(lista);
+
+newSet.add("Carlos")
+newSet.add("JavaScript");
+console.log(newSet)
+
+
